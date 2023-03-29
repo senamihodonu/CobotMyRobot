758cd5e2de7e8efb37f58f6d411c466be80c2d99
Senami Hodonu <84221854+senamihodonu@users.noreply.github.com>
2023-03-29 16:19:58 -0600

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from pymycobot.mypalletizer import MyPalletizer
 # for myBuddy
 from pymycobot.mybuddy import MyBuddy
 
-coords = mc.get_coords()print(coords)
+coords = mc.get_coords()
+print(coords)
 mycobot = MyCobot("/dev/ttyAMA0", 115200)
 mycobot.send_angles([0,0,0,0,0,0], 80)
 mycobot.send_coords([160, 160, 160, 0, 0, 0], 70, 0)
