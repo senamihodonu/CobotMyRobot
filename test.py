94ea508c180125da697d82deb332be73b2ad8375
Senami Hodonu <84221854+senamihodonu@users.noreply.github.com>
2023-03-29 16:18:41 -0600

    Update test.py

--- test.py
+++ test.py
@@ -10,4 +10,4 @@ from pymycobot.mybuddy import MyBuddy
 coords = mc.get_coords()print(coords)
 mycobot = MyCobot("/dev/ttyAMA0", 115200)
 mycobot.send_angles([0,0,0,0,0,0], 80)
-mycobot.send_angles([1,1,1,0,0,0], 80)
+mycobot.send_coords([160, 160, 160, 0, 0, 0], 70, 0)
