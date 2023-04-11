a3ae5d5c05cd1bfbe0cc405c466c4f63f6e280a9
iRETI <shodonu@outlook.com>
2023-04-11 12:31:50 -0600

    test2

---
+++ test2.py
@@ -0,0 +1,15 @@
+# for mycobot,mecharm
+from pymycobot.mycobot import MyCobot
+
+# for mypalletizer
+from pymycobot.mypalletizer import MyPalletizer
+
+# for myBuddy
+from pymycobot.mybuddy import MyBuddy
+
+
+mycobot = MyCobot("/dev/ttyAMA0", 115200)
+coords = mycobot.get_coords()
+print(coords)
+mycobot.send_angles([0,0,0,0,0,0], 80)
+# mycobot.send_coords([160, 160, 160, 0, 0, 0], 70, 0)
