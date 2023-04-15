05657011f7ec6766382179bd2bf9707422be3cdd
iRETI <shodonu@outlook.com>
2023-04-14 20:27:56 -0600

    additons

--- cobot_action_server.py
+++ cobot_action_server.py
@@ -25,14 +25,13 @@ class SetAngleActionServer(Node):
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
 
-
         ####
         mycobot.set_gripper_state(0, 50)
         mycobot.wait(3)
         mycobot.send_angles(goal_handle.request.angles, goal_handle.request.speed)
         mycobot.wait(1)
         mycobot.send_coords([288.9,-87.5,190.9,167.43,0.6,-90.22], 50, 0)
-        mycobot.send_coords([288.9,-87.5,190.9,151.64,0.6,-90.22], 50, 0)
+        mycobot.send_coords([288.9,-87.5,190.9,150,0.6,-90.22], 50, 0)
         mycobot.wait(5)
         mycobot.set_gripper_state(1, 50)
         ####
